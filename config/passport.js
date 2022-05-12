30dc77950d144bd317bccb25e844361d98fca5d7
Douglas <88936801+dougovski@users.noreply.github.com>
2022-05-12 20:12:14 -0300

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,26 @@
 var passport = require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
+var mongoose = require('mongoose');
 
 module.exports = function() {
 
+  var Usuario = mongoose.model('Usuario');
+  
   passport.use(new GitHubStrategy({
   clientID: 'dswa5-11-ac-pt3008576',
   clientSecret: 'Palmeiras_2022',
   callbackURL: 'https://dswa5-11-ac-pt3008576.herokuapp.com/auth/github/callback'
    }, function(accessToken, refreshToken, profile, done) {
-    }));
-  };
+        Usuario.findOrCreate(
+        { "login" : profile.username},
+        { "nome" : profile.username},
+        function(erro, usuario) {
+        if(erro)
+        console.log(erro);
+        return done(erro);
+  }
+        return done(null, usuario);
+      }
+    );
+  }));
+};
