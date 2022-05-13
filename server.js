8da83f7567f677369b63a51efe0e26d05823014a
Douglas <88936801+dougovski@users.noreply.github.com>
2022-05-12 21:02:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var express = require('express');
+var app = express();
 var app = require('./config/express')();
+require('./config/express')(app);
+require('./config/passport')();
+
 const url = 'mongodb+srv://dougovski:1409@clusterdhaps.rzyac.mongodb.net/ifsp?retryWrites=true&w=majority';
 require('./config/database.js')(url);
+
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express Server escutando na porta ' + app.get('port'));
 });
