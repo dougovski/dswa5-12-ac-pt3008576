100c1cb8fea17edb40d377758e8eb8c655ca8a69
Douglas <88936801+dougovski@users.noreply.github.com>
2022-05-19 19:37:26 -0300

    Update auth.js

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,18 +1,26 @@
+var passport = require('passport');
+
 module.exports = function(app) {
+
     app.get('/auth/github', passport.authenticate('github'));
+
     app.get('/auth/github/callback',
     passport.authenticate('github', {
-    successRedirect: '/'
+        successRedirect: '/'
     }));
+
     app.get('/', function(req, res, next) {
         if(req.isAuthenticated()) {
+        // permite que outras rotas sejam processadas
             return next();
         } else {
-        res.render("auth");
+        // renderiza auth.ejs
+            res.render("auth");
         }
     });
+
     app.get('/logout', function(req, res){
-        req.logOut;
+        req.logOut();
         res.redirect('/');
     });
 }
