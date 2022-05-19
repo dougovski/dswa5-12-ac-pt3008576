ebe1c85c273a8f5f0cdcf313e3ed9f2716af841f
Douglas <88936801+dougovski@users.noreply.github.com>
2022-05-18 21:54:53 -0300

    Update consulta.js

--- consulta.js
+++ consulta.js
@@ -1,30 +1,27 @@
-const findDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('contatos');
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-        assert.equal(err, null);
-        console.log("Foram encontrados os seguintes docs:");
-        console.log(docs)
-        callback(docs);
-    });
+const {MongoClient} = require('mongodb');
+
+async function main() {
+	const uri = "mongodb+srv://dougovski:1409@cluster0.xvyvf.mongodb.net/ifsp?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+
+        await listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-// Connection URL
-const url = 'mongodb+srv://dswa5ifsp:senha@clusterdswa5-x96ld.mongodb.net/test?retryWrites=true&w=majority';
-// Database Name
-const dbName = 'ifsp';
-// Use connect method to connect to the server
-MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
-    assert.equal(null, err);
-    console.log("Aluno: Fabio Teixeira");
-    console.log("Servidor conectado!");
-
-    const db = client.db(dbName);
-
-    findDocuments(db, function() {
-        client.close();
-    });
-
-});
