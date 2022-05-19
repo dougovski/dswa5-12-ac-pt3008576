f3671f050d1dfc0a644a90fb8d1490b3f84e593a
Douglas <88936801+dougovski@users.noreply.github.com>
2022-05-19 20:14:55 -0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var load = require('express-load');
+
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
