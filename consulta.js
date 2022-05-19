c76ff0bd7860e183e20e1fc9f7b0c356a3f21e85
Douglas <88936801+dougovski@users.noreply.github.com>
2022-05-19 19:50:18 -0300

    Update consulta.js

--- consulta.js
+++ consulta.js
@@ -1,27 +1,27 @@
-const {MongoClient} = require('mongodb');
+console.log('Aluno: João Pedro Soares Ferreira');
+
+const { MongoClient } = require('mongodb');
 
 async function main() {
-	const uri = "mongodb+srv://dougovski:1409@clusterdhaps.rzyac.mongodb.net/ifsp?retryWrites=true&w=majority";
+   const uri =
+      'mongodb+srv://dougovski:1409@clusterdhaps.rzyac.mongodb.net/ifsp?retryWrites=true&w=majority';
 
-    const client = new MongoClient(uri);
+   const client = new MongoClient(uri);
 
-    try {
-        await client.connect();
+   try {
+      await client.connect();
+      console.log('Servidor Conectado!');
+      const database = client.db('ifsp');
 
-        await listDatabases(client);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+      const contatos = database.collection('contatos');
+
+      const findResult = await contatos.find({}).toArray();
+      console.log('Foram encontrados os seguintes docs:');
+      console.log(findResult);
+   } catch (e) {
+      console.error(e);
+   } finally {
+      await client.close();
+   }
 }
-
 main().catch(console.error);
-
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
